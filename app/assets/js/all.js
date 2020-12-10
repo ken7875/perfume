a86f958726c686dfb4cbb4802f6c2385bc6b2bd7
Ken Zou <ken19970723@gmail.com>
2020-12-10 21:37:13 +0800

    登入優化

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,9 +5,18 @@ $(document).ready(() => {
     (() =>{
         if(location.href.split('/')[3] === 'logIn.html'){
             var submit = document.querySelector('.logInBtn');
+            var account = document.querySelector('.account');
+            var password = document.querySelector('.password');
             submit.addEventListener('click', () =>{
-                
+                if(account.value ==='' && password.value === ''){
+                    alert('請填入您的帳號與密碼')
+                }else if(password.value === ''){
+                    alert('請填入您的密碼')
+                }else if(account.value === ''){
+                    alert('請填入您的帳號')
+                }else{
                     location.href = '/account.html'
+                }
             })
         }
     })()
