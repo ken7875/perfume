d5011a9023ba759ca8abe6782e6eae7ed9bbbedc
Ken Zou <ken19970723@gmail.com>
2020-12-13 12:07:59 +0800

    按鈕連結

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,23 +1,5 @@
 console.log('Hello!');
 
 $(document).ready(() => {
-    // logIn登入按鈕
-    (() =>{
-        if(location.href.split('/')[3] === 'logIn.html'){
-            var submit = document.querySelector('.logInBtn');
-            var account = document.querySelector('.account');
-            var password = document.querySelector('.password');
-            submit.addEventListener('click', () =>{
-                if(account.value ==='' && password.value === ''){
-                    alert('請填入您的帳號與密碼')
-                }else if(password.value === ''){
-                    alert('請填入您的密碼')
-                }else if(account.value === ''){
-                    alert('請填入您的帳號')
-                }else{
-                    location.href = '/account.html'
-                }
-            })
-        }
-    })()
+    
 });
