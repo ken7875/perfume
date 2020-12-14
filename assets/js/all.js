9ee6fa2fbf695c2358c516449673d56b6fade3cb
Ken Zou <ken19970723@gmail.com>
2020-12-14 12:06:35 +0800

    Update 2020-12-14T04:06:28.363Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,5 @@
 "use strict";
 
 console.log('Hello!');
-$(document).ready(function () {
-  // logIn登入按鈕
-  (function () {
-    if (location.href.split('/')[3] === 'logIn.html') {
-      var submit = document.querySelector('.logInBtn');
-      submit.addEventListener('click', function () {
-        location.href = '/account.html';
-      });
-    }
-  })();
-});
+$(document).ready(function () {});
 //# sourceMappingURL=all.js.map
